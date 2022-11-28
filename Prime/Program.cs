8617925fe380b960d4bea316bb72e5e4b264742a
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-11-28 15:19:08 +0000

    what the **** is this piece of ****

--- Prime/Program.cs
+++ Prime/Program.cs
@@ -4,32 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Prime
 {
     class Program
     {
-        static int min = 1;
+        static int min = 2;
         static long max;
         static bool ifPrime;
         static Stopwatch sw = new Stopwatch();
         static List<long> primes = new List<long>();
         static Random rndm = new Random();
+        static bool firstCurrentlyPrime;
+        static bool secondCurrentlyPrime;
+
+        static int currentNum;
+
+        static Task firstHalfThread;
+        static Task secondHalfThread;
 
         static void Main(string[] args)
         {
+            //Console.WriteLine("What is the minimum? ");
+            //min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the maximum? ");
+            max = long.Parse(Console.ReadLine());
+
             sw.Start();
             Task();
             sw.Stop();
 
             Console.WriteLine($"The time that has passed is {sw.Elapsed}");
-            Console.WriteLine($"The result of the check was that the primes were: {Check()}");
+            //Console.WriteLine($"The result of the check was that the primes were: {Check()}");
             Console.ReadLine();
+        }
+
+        static void ThreadedPrime()
+        {
+            firstCurrentlyPrime = false;
+            secondCurrentlyPrime = false;
+            firstHalfThread = new Task(checkFirst);
+            secondHalfThread = new Task(checkSecond);
+            firstHalfThread.Start();
+            secondHalfThread.Start();
+
+            while (!(firstHalfThread.IsCompleted && secondHalfThread.IsCompleted)) { /*Console.WriteLine("Waiting");*/ }
+            Console.WriteLine($"{currentNum} {firstHalfThread.IsCompleted} { secondHalfThread.IsCompleted}");
+            if (firstCurrentlyPrime && secondCurrentlyPrime)
+            {
+                primes.Add(currentNum);
+            }
+
+            //Console.WriteLine($"Is {i} prime: {ifPrime}");
         }
 
         static bool isPrime(long num)
         {
-            for (int a = 0; a < primes.Count() - 1 && primes[a] < Math.Sqrt(num); a++)
+            for (int a = 0; a < primes.Count() - 1 && primes[a] <= Math.Sqrt(num); a++)
             {
                 if (num % primes[a] == 0)
                 {
@@ -42,20 +75,42 @@ namespace Prime
 
         static void Task()
         {
-            //Console.WriteLine("What is the minimum? ");
-            //min = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("What is the maximum? ");
-            max = long.Parse(Console.ReadLine());
-
             for (int i = min; i <= max; i++)
             {
-                ifPrime = isPrime(i);
+                currentNum = i;
+                Console.WriteLine(currentNum);
+                ThreadedPrime();
                 //Console.WriteLine($"Is {i} prime: {ifPrime}");
             }
         }
 
-        static bool SlowIsPrime(long num)
+        static void checkFirst()
+        {
+            for (int a = 0; a < (primes.Count() - 1) / 2 && primes[a] <= Math.Sqrt(currentNum); a++)
+            {
+                if (currentNum % primes[a] == 0)
+                {
+                    firstCurrentlyPrime = false;
+                }
+            }
+            firstCurrentlyPrime = true;
+        }
+
+        static void checkSecond()
+        {
+            int halfLen = (primes.Count() - 1) / 2;
+            for (int a = 0; a < halfLen && primes[a + halfLen] <= Math.Sqrt(currentNum); a++)
+            {
+                if (currentNum % primes[a] == 0)
+                {
+                    secondCurrentlyPrime = false;
+                }
+            }
+            secondCurrentlyPrime = true;
+        }
+
+
+        /*static bool SlowIsPrime(long num)
         {
             for (int i = 2; i <= Math.Sqrt(num);i ++)
             {
@@ -76,6 +131,6 @@ namespace Prime
                 listCorrect = SlowIsPrime(primes[num]);
             }
             return listCorrect;
-        }
+        }*/
     }
 }
